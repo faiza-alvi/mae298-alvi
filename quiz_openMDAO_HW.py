cdcb6ac01b4b306331a4b7ac477c80a26a9f3c7c
faiza-alvi <bookisharchitect@gmail.com>
2025-10-07 15:59:37 -0700

    Modify for test problem in class

--- quiz_openMDAO_HW.py
+++ quiz_openMDAO_HW.py
@@ -22,21 +22,22 @@ import openmdao.api as om
 # build the model
 prob = om.Problem()
 
-prob.model.add_subsystem('paraboloid', om.ExecComp('f = (x-4)**2 + x*y + (y+3)**2 - 3'))
+# prob.model.add_subsystem('paraboloid', om.ExecComp('f = (x-4)**2 + x*y + (y+3)**2 - 3'))
+prob.model.add_subsystem('paraboloid', om.ExecComp('f = 0.1*(x+y) - abs( sin(x)*cos(y)*exp( ( 1 -power(( power(x,2) + power(y,2) ),0.5 ) ) /pi ) )'))
 
 # setup the optimization
 prob.driver = om.ScipyOptimizeDriver()
-prob.driver.options['optimizer'] = 'Nelder-Mead'
+prob.driver.options['optimizer'] = 'SLSQP'
 
-prob.model.add_design_var('paraboloid.x', lower=-50, upper=50)
-prob.model.add_design_var('paraboloid.y', lower=-50, upper=50)
+prob.model.add_design_var('paraboloid.x', lower=-1000, upper=50)
+prob.model.add_design_var('paraboloid.y', lower=-1000, upper=50)
 prob.model.add_objective('paraboloid.f')
 
 prob.setup()
 
 # Set initial values.
-prob.set_val('paraboloid.x', 3.0)
-prob.set_val('paraboloid.y', -4.0)
+prob.set_val('paraboloid.x', 0.0)
+prob.set_val('paraboloid.y', 0.0)
 
 # run the optimization
 prob.run_driver()
